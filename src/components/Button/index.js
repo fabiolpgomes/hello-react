51ce66dccf54ea503426f9e92c1d358507122790
Fabio Gomes <fabiolpgomes@hotmail.com>
2022-10-30 16:59:57 +0100

    Add info

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,7 +3,7 @@ import "./button.css";
 function Button() {
   return (
     <>
-      <a className="button" href="http://reactjs.org/">Awesome! Go React</a>
+      <a className="button" href="http://reactjs.org/">Awesome, Go React!!!</a>
       
       
     </>
